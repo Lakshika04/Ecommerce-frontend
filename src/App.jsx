2331edc9b2db37c6e734c714bdd48524f3c23862
Lakshika04 <lakshikabourai@gmail.com>
2025-10-07 23:20:48 +0530

    hero and popular section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Shop from './Pages/Shop'
+import ShopCategory from './Pages/ShopCategory'
+import LoginSignup from './Pages/LoginSignup'
+import Product from './Pages/Product'
+import Cart from './Pages/Cart'
+
 
 const App = () => {
   return (
-    <div className='text-4xl border-4 bg-red-300 m-4 p-4 border-amber-400'>App</div>
+    <div >
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/men' element={<ShopCategory category="men"/>}/>
+        <Route path='/women' element={<ShopCategory category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory category="kids"/>}/>
+        <Route path='/product' element={<Product/>}>
+         <Route path=':productId' element={<Product/>}/>
+         </Route>
+         <Route path='/cart' element ={<Cart/>}/>
+         <Route path='/login' element ={<LoginSignup/>}/>
+
+      </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
